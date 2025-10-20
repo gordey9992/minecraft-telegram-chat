3b6fb2ed7eec834997f48dac5ecba2d4256a5b08
gordey9992 <146545641+gordey9992@users.noreply.github.com>
2025-10-20 13:47:22 +0300

    Update ChatListener.java

--- src/main/java/com/github/chatsync/listeners/ChatListener.java
+++ src/main/java/com/github/chatsync/listeners/ChatListener.java
@@ -47,7 +47,7 @@ public class ChatListener implements Listener {
         }
         
         // Получаем префикс игрока
-        String prefix = ChatFormatter.getSimplePlayerPrefix(player);
+        String prefix = ChatFormatter.getPlayerPrefix(player);
         String playerName = ChatFormatter.getPlayerDisplayName(player);
         
         // Отправляем сообщение в Telegram
